cbfebec9bc0ac721ec3734c01b17cfafb07c4a4c
Ahmet-Sarraf <srrfahmet@gmail.com>
2026-03-16 23:31:54 +0300

    duzenleme(config): auth, storage ve supabase modulleri ana app modulune dahil edildi

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { SupabaseModule } from './supabase/supabase.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { StorageModule } from './storage/storage.module';
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SupabaseModule,
+    PrismaModule,
+    AuthModule,
+    StorageModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
