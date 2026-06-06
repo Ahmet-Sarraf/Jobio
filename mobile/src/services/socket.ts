8e1b7b2df8a481752a6d464ec843ac884df477e1
Ahmet-Sarraf <srrfahmet@gmail.com>
2026-06-06 18:01:48 +0300

    feat(mobile): socket.io kutuphanesi ve singleton soket servisi kurulumu

---
+++ mobile/src/services/socket.ts
@@ -0,0 +1,32 @@
+import { io, Socket } from 'socket.io-client';
+
+let socket: Socket | null = null;
+
+export const getSocketAsync = (token: string | null): Socket | null => {
+  if (socket) {
+    return socket;
+  }
+
+  if (!token) return null;
+
+  const apiUrl = process.env.EXPO_PUBLIC_API_URL || 'http://10.175.205.165:3000';
+
+  // Sockets are connected to the root domain of the api url
+  // If EXPO_PUBLIC_API_URL contains a path, we strip it out (e.g. replace '/api', '')
+  const socketUrl = apiUrl.replace(/\/api$/, '');
+
+  socket = io(socketUrl, {
+    auth: { token: `Bearer ${token}` },
+    transports: ['websocket'],
+    autoConnect: false, // We will connect manually
+  });
+
+  return socket;
+};
+
+export const disconnectSocket = () => {
+  if (socket) {
+    socket.disconnect();
+    socket = null;
+  }
+};
